edea1de4235052bd6a643b59145882651af0acdd
sabin <sabinkarki@Sabins-MacBook-Air.local>
2024-04-23 13:56:12 +0545

    Vue Package

---
+++ VueCheckk/src/main.js
@@ -0,0 +1,17 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+// main.js in your new project
+import { createApp} from 'vue';
+import App from 'vuetodomodel/src/App.vue';
+import 'vuetodomodel/src/main.css';
+createApp(App).mount('#app');
+
+
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount('#app');
